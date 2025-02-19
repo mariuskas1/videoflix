3d64a9e58c2c29b03c204772b895a1884f1284ca
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-19 17:10:08 +0100

    basic signup route

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  userInput:string = '';
+
+  constructor(private router: Router){}
+
+
+  checkUserInput(){
+    if(this.userInput?.length > 1){
+      this.router.navigate(['/signup']);
+    }
+  }
+
 
 }
