b3bdf1bda6ad9b7e0d327873c3cae85a3e356f8b
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-20 14:25:15 +0100

    forgot password page

--- src/app/auth/forgot-pw/forgot-pw.component.ts
+++ src/app/auth/forgot-pw/forgot-pw.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-pw',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './forgot-pw.component.html',
   styleUrl: './forgot-pw.component.scss'
 })
 export class ForgotPwComponent {
+  email = '';
+
+
+  onSubmit(ngForm: NgForm){
+    //reset password
+  }
 
 }
