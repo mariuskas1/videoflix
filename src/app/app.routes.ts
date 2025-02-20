23ed986470821d01f28e0b0fc7703f156fe3b59b
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-20 14:36:15 +0100

    added main-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ForgotPwComponent } from './auth/forgot-pw/forgot-pw.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'forgot-pw', component: ForgotPwComponent },
-
+    { path: 'main', component: MainPageComponent },
 ];
