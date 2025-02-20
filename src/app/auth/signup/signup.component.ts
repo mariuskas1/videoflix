0558b252e59a907b2b6f02164c2f1a9327dfe575
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-20 10:56:14 +0100

    sign up page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  showPassword1 = false;
+  showPassword2 = false;
+  pwsDontMatch = false;
 
+
+  togglePassword1Visibility(){
+    this.showPassword1 = !this.showPassword1;
+  }
+
+  togglePassword2Visibility(){
+    this.showPassword2 = !this.showPassword2;
+  }
 }
