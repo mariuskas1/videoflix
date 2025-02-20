6bfaded6457f5b284e6da8c8e049c2256fc623bf
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-20 13:55:32 +0100

    signup form validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,6 +15,13 @@ export class SignupComponent {
   showPassword1 = false;
   showPassword2 = false;
   pwsDontMatch = false;
+  pwsTooShort = false;
+
+  signupData = {
+    email : '',
+    password : '',
+    repeated_pw : ''
+  }
 
 
   togglePassword1Visibility(){
@@ -24,4 +31,35 @@ export class SignupComponent {
   togglePassword2Visibility(){
     this.showPassword2 = !this.showPassword2;
   }
+
+
+  onSubmit(ngForm: NgForm){
+    this.checkPasswords();
+    if(this.pwsDontMatch || this.pwsTooShort){
+      return;
+    }
+
+    if(ngForm.valid){
+      console.log(this.signupData);
+      
+      setTimeout(()=>{
+        ngForm.resetForm();
+      }, 200)
+    }
+  }
+
+
+  checkPasswords(){
+    this.pwsDontMatch = false;
+    this.pwsTooShort = false;
+
+    if(this.signupData.password !== this.signupData.repeated_pw){
+      this.pwsDontMatch = true;
+    }
+
+    if(this.signupData.password.length < 6){
+      this.pwsTooShort = true;
+    }
+  }
+
 }
