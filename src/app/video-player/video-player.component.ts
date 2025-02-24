54f19db5ded8685374c92220d1579281d8087ef6
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-24 17:35:22 +0100

    play video functionality

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,22 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { Video } from '../models/video.model';
 import { ActivatedRoute } from '@angular/router';
+import videojs from 'video.js';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-video-player',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './video-player.component.html',
   styleUrl: './video-player.component.scss'
 })
 export class VideoPlayerComponent {
   BASE_URL = 'http://127.0.0.1:8000/api/videos';
   video!: Video;
+  videoLoaded = false;
+
+  @ViewChild('videoPlayer', { static: true }) videoElement!: ElementRef<HTMLVideoElement>;
+  player!: any;
 
   constructor(private route: ActivatedRoute){}
 
   ngOnInit(){
     this.loadVideo();    
+  }
+
+  ngAfterViewInit(): void {
+    this.player = videojs(this.videoElement.nativeElement, {
+      controls: true, // Enable video controls
+      autoplay: true, // Automatically play when loaded
+      fluid: true, // Responsive video sizing
+      preload: 'auto' // Preload the video
+    });
+
+    
+  }
+
+  ngOnDestroy(): void {
+    if (this.player) {
+      this.player.dispose(); // Cleanup the player when component is destroyed
+    }
   }
 
 
@@ -28,6 +52,7 @@ export class VideoPlayerComponent {
         const response = await fetch(videoURL);
         if(response.ok){
           this.video = await response.json();
+          this.videoLoaded = true;
           console.log("Fetched video:", this.video);
         }
       } catch (error) {
