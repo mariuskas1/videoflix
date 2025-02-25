9899d796d6405dee100db76fc9a9bc9c6d584d04
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-25 13:26:59 +0100

    added header to player

--- src/app/vjs-player/vjs-player.component.ts
+++ src/app/vjs-player/vjs-player.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import { Router } from '@angular/router';
 import videojs from 'video.js';
 
 
@@ -28,7 +29,7 @@ export class VjsPlayerComponent {
   player: any;
 
 
-  constructor(private elementRef: ElementRef){}
+  constructor(private elementRef: ElementRef, private router: Router){}
 
 
 
@@ -42,21 +43,73 @@ export class VjsPlayerComponent {
       console.log('onPlayerReady', this);
     });
     this.addTitleToControlBar();
+    this.addHeaderToPlayer();
   }
 
   addTitleToControlBar() {
     if (!this.player) return;
 
     const controlBar = this.player.getChild('controlBar'); 
+    const volumePanel = controlBar.getChild('volumePanel');
 
     if (controlBar) {
         const titleElement = document.createElement('div');
         titleElement.className = 'vjs-video-title';
         titleElement.innerText = this.videoTitle; 
+        titleElement.style.flexGrow = '1';
 
-        controlBar.el().insertBefore(titleElement, controlBar.el().firstChild);
+        volumePanel.el().insertAdjacentElement('afterend', titleElement);
     }
-}
+  }
+
+
+  addHeaderToPlayer(){
+    if (!this.player) return;
+
+    const playerElement = this.player.el(); // Get Video.js container
+    if (!playerElement) return;
+
+    // Create the header element
+    const headerElement = document.createElement('div');
+    headerElement.className = 'vjs-video-header';
+    headerElement.innerHTML = `
+      <img src="./../../assets/img/arrow_back.png" class="arrow-back" (click)="returnToMainPage()">
+      <img src="./../../assets/img/logo_small.png">
+    `;
+
+    // Append header to the player
+    playerElement.appendChild(headerElement);
+    const arrowBackElement = headerElement.querySelector('.arrow-back');
+    if (arrowBackElement) {
+      arrowBackElement.addEventListener('click', () => this.returnToMainPage());
+    }
+
+    // Handle hiding/showing header based on user activity
+    this.setupHeaderVisibility(headerElement);
+  }
+
+
+  setupHeaderVisibility(headerElement: HTMLElement) {
+    if (!this.player) return;
+
+    let timeout: any;
+
+    const showHeader = () => {
+        headerElement.classList.add('visible');
+        clearTimeout(timeout);
+        timeout = setTimeout(() => headerElement.classList.remove('visible'), 2000); // Hide after 2s
+    };
+
+    // Show header when user interacts
+    this.player.on('mousemove', showHeader);
+    this.player.on('touchstart', showHeader);
+  }
+
+  returnToMainPage(){
+    console.log('hel')
+    this.router.navigate(['/main']);
+  }
+  
 
 
   ngOnDestroy(){
