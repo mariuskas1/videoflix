ccff71b836fb34beb37a219df4c6f300fc35e8cf
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-25 15:44:25 +0100

    added policy components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,16 @@ import { ForgotPwComponent } from './auth/forgot-pw/forgot-pw.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { authGuard } from './auth/guards/auth.guard';
+import { PrivacyPolicyComponent } from './policy/privacy-policy/privacy-policy.component';
+import { ImprintComponent } from './policy/imprint/imprint.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'forgot-pw', component: ForgotPwComponent },
+    { path: 'policy', component: PrivacyPolicyComponent },
+    { path: 'imprint', component: ImprintComponent },
     { path: 'main', component: MainPageComponent, canActivate: [authGuard] },
     { path: 'play/:id', component: VideoPlayerComponent, canActivate: [authGuard] },
 ];
