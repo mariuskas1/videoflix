8f11e13a5df30f0cfa17cc188ad810445cc38f1d
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-04 15:08:23 +0100

    added toast component

--- src/app/auth/forgot-pw/forgot-pw.component.ts
+++ src/app/auth/forgot-pw/forgot-pw.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToastComponent } from '../../shared/toast/toast.component';
 
 @Component({
   selector: 'app-forgot-pw',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
+  imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule, ToastComponent],
   templateUrl: './forgot-pw.component.html',
   styleUrl: './forgot-pw.component.scss'
 })
