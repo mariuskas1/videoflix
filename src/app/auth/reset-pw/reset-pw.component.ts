86e93d97ed3ec956acf3433fee2cc7ffa8e51287
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-05 16:55:00 +0100

    reset pw component layout

--- src/app/auth/reset-pw/reset-pw.component.ts
+++ src/app/auth/reset-pw/reset-pw.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { ToastComponent } from '../../shared/toast/toast.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-pw',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, ToastComponent, CommonModule, FormsModule],
   templateUrl: './reset-pw.component.html',
   styleUrl: './reset-pw.component.scss'
 })
 export class ResetPwComponent {
+  showToastMessage = false;
+  toastMessage = 'Your password has been successfully reset.';
+
+  showPassword1 = false;
+  showPassword2 = false;
+  pwsDontMatch = false;
+  pwsTooShort = false;
+
+  signupData = {
+    email : '',
+    password : '',
+    repeated_password : ''
+  }
+
+
+
+  togglePassword1Visibility(){
+    this.showPassword1 = !this.showPassword1;
+  }
+
+  togglePassword2Visibility(){
+    this.showPassword2 = !this.showPassword2;
+  }
+
+
+  onSubmit(ngForm: NgForm){
+    
+  }
 
 }
