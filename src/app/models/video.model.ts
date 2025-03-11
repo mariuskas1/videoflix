d85da9a78fc79dae3a9a78a2940e5f20854eb3a0
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-11 10:01:53 +0100

    expanded video interface to include different resolutions

--- src/app/models/video.model.ts
+++ src/app/models/video.model.ts
@@ -8,4 +8,8 @@ export interface Video {
     thumbnail?: string; 
     new: boolean;
 
+    video_file_120p: string;
+    video_file_360p: string;
+    video_file_720p: string;
+    video_file_1080p: string;
 }
